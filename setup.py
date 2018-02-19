70329fb3990de1680a851ff25cfffdd53831dbd2
rasztul <rasztul@gmail.com>
2018-02-19 13:26:29 -0600

    Se corrige la ruta de la carpeta donde se encuentran las librerias

--- setup.py
+++ setup.py
@@ -4,13 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=[
-        'openag_brain',
-        'openag_lib',
-        'openag_lib.firmware',
-        'openag_lib.firmware.plugins',
-        #'openag_lib.db_bootstrap',
-       # 'openag_brain.commands',
-       # 'openag_brain.peripherals'
+        'libs',
     ],
     package_dir={'': 'src'},
 )
