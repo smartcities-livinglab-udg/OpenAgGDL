45f308ca6c40ac181e5f056df749c888d6a77bf7
rasztul <rasztul@gmail.com>
2018-02-21 18:11:28 -0600

    Se modifica launcher para publicar 2 topicos en el mismo nodo htu21d. Se agrega nodo de intensidad de luz

---
+++ nodes/sensor_tsl2561.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import rospy
+# mensajes tipo Int
+#from openaggdl.msg import IntR
+from std_msgs.msg import Float64
+# ------- Sensores
+"""[requiered]sudo pip install tsl2561"""
+"""[requiered]sudo pip install adafruit_gpio"""  
+from tsl2561 import TSL2561
+    
+if __name__ == '__main__':
+    rospy.init_node("sensor_tsl2561")
+    rate = rospy.get_param("~rate_hz", 1)
+    r = rospy.Rate(rate)
+
+    sensor = HTU21D()
+
+    tsl2561_pub = rospy.Publisher("light_intensity/raw", Float64, queue_size=10)
+    
+    while not rospy.is_shutdown():
+
+        data = sensor.lux()
+        
+        if data is not None:
+            tsl2561_pub.publish(data)
+        
+        r.sleep()
